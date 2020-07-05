78b1ae1a5ce263d4f9ea8bd53bd3a86ba3830cd6
qiufengming <878189687@qq.com>
2020-07-05 19:28:00 +0800

    第一次提交

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import First from '@/components/views/First'
+import Second from '@/components/views/Second'
+import Ueditor from '@/pages/ueditor/Ueditor'
+import docStructure from '@/pages/works/docStructure/index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld', 
+      component: HelloWorld
+    }, {
+      path: '/first',
+      name: 'First',
+      component: First
+    }, {
+      path: '/second',
+      name: 'Second',
+      component: Second
+    }, {
+      path: '/ueditor',
+      name: 'Ueditor',
+      component: Ueditor
+    }, {
+      path: '/works/docstructure',
+      name: 'docStructure',
+      component: docStructure
+    }
+  ]
+})
